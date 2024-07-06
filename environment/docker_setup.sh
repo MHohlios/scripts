582ff74616d27b0971757cdc8a4f035d1bbf986d
mhohlios-sage <mhohlios@sagesocial.com>
2024-07-06 14:35:09 -0400

    Fix: Removing sudo run requirement from docker script

--- environment/docker_setup.sh
+++ environment/docker_setup.sh
@@ -1,24 +1,24 @@
 #/bin/bash
-if [ `id -u` -ne 0 ]
-  then echo Please run this script as root or using sudo!
+if [ `id -u` -eq 0 ]
+  then echo Please do not run this scirpt as root!
   exit
 fi
 
 # Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-usermod -a -G docker $USER
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -a -G docker $USER
 
 echo 'Installation complete!'
 echo 'Please run "newgrp docker" or log out and log back in to apply the changes'
